54cd5165941b3df390e0282a5e321746f7a5826a
Michael Huensch <mrh@run00.com>
2013-05-26 21:40:41 -0500

    Adding replace first

--- Run00.Utilities/ExtensionsForString.cs
+++ Run00.Utilities/ExtensionsForString.cs
@@ -8,14 +8,30 @@ namespace Run00.Utilities
 		/// Replaces the last occurrence of the string with the given value.
 		/// </summary>
 		/// <param name="source">The source.</param>
-		/// <param name="lookFor">The look for.</param>
-		/// <param name="replaceWith">The replace with.</param>
+		/// <param name="lookFor">The string to look for.</param>
+		/// <param name="replaceWith">The string to replace the lookfor with.</param>
 		/// <returns></returns>
 		public static string ReplaceLast(this string source, string lookFor, string replaceWith)
 		{
 			var index = source.LastIndexOf(lookFor);
 			var length = index == -1 ? source.Length : index;
 			return source.Substring(0, length);
+		}
+
+		/// <summary>
+		/// Replaces the first occurrence of the string with the given value.
+		/// </summary>
+		/// <param name="source">The source.</param>
+		/// <param name="lookFor">The string to look for.</param>
+		/// <param name="replaceWith">The string to replace the lookfor with.</param>
+		/// <returns></returns>
+		public static string ReplaceFirst(this string source, string lookFor, string replaceWith)
+		{
+			var index = source.IndexOf(lookFor);
+			if (index == -1)
+				return source;
+
+			return source.Substring(index, source.Length);
 		}
 
 		/// <summary>
